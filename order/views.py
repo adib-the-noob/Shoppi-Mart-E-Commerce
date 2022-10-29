e754cbd7e4683cfd9e39ab01203ec623fe1fb865
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-29 23:46:32 +0600

    Delete Cart

--- order/views.py
+++ order/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet,GenericViewSet
+from rest_framework.viewsets import (
+    ModelViewSet,
+    GenericViewSet,
+)
+from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+)
+
 from .models import Cart
 from .serializers import CartSerializer
 # Create your views here.
 
-class CartViewSet(ModelViewSet,GenericViewSet):
+class CartViewSet(  GenericViewSet,
+                    ListModelMixin,
+                    RetrieveModelMixin,
+                    CreateModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin
+                    ):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
