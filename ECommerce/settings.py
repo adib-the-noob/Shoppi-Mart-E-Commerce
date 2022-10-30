4a08d7f91133284fac0fee54e6773df3a3bd0458
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-30 22:13:22 +0600

    Static File Added

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -128,9 +128,9 @@ AUTH_USER_MODEL = 'user.MyUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
