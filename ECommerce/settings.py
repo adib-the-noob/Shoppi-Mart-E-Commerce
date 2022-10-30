cf453191ee0089edaca65869f297f63179e33e2e
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-30 22:10:09 +0600

    Allowed Host added

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yz@s8&t0-*)+tb)828n53(aae%asvzl5s=^y_9bjhg534sn)lw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
