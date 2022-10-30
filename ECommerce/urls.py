85e32d50e05221232016d7bd7c8244cfe697c170
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-30 22:28:44 +0600

    Cloud Fixed

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
@@ -25,4 +26,4 @@ urlpatterns = [
     path('', include('order.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
